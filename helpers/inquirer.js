336130afd5d64182a77f13de52e87c888f7e413f
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-07-15 17:56:39 +0200

    feat: :art: Mostrando listado de opciones y de información a rellenar

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,27 +1,54 @@
 import inquirer from 'inquirer';
 import colors from 'colors';
 
-const menuOpts = [
+const preguntas = [
     {
         type: 'list',
         name: 'opcion',
-        message: 'Seleccione una opción',
+        message: '¿Qué desea hacer?',
         choices: [
             {
                 value: 1,
-                name: `${'1.'.green} Buscar Ciudad`
+                name: `${ '1.'.green } Buscar ciudad`
             },
             {
                 value: 2,
-                name: `${'2.'.green} Historial`
+                name: `${ '2.'.green } Historial`
             },
             {
-                value: 3,
-                name: `${'3.'.green} Salir`
+                value: 0,
+                name: `${ '0.'.green } Salir`
             },
-        ],
-    },
+        ]
+    }
 ];
+
+const inquirerMenu = async() => {
+
+    console.clear();
+    console.log('=========================='.green);
+    console.log('  Seleccione una opción'.white );
+    console.log('==========================\n'.green);
+
+    const { opcion } = await inquirer.prompt(preguntas);
+
+    return opcion;
+}
+
+const pausa = async() => {
+    
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${ 'enter'.green } para continuar`
+        }
+    ];
+
+    console.log('\n');
+    await inquirer.prompt(question);
+}
+
 const leerInput = async( message ) => {
 
     const question = [
@@ -41,31 +68,82 @@ const leerInput = async( message ) => {
     const { desc } = await inquirer.prompt(question);
     return desc;
 }
-const inquirerMenu = async () => {
-    console.clear();
-    console.log('==========================='.green);
-    console.log('   Seleccione una opción'.green);
-    console.log('===========================\n'.green);
 
-    const opt = await inquirer.prompt(menuOpts);
+const listarLugares = async( lugares = [] ) => {
 
-    return opt;
-};
-const pausa = async() => {
-    
+    const choices = lugares.map( (lugar, i) => {
+
+        const idx = `${i + 1}.`.green;
+
+        return {
+            value: lugar.id,
+            name:  `${ idx } ${ lugar.nombre }`
+        }
+    });
+
+    choices.unshift({
+        value: '0',
+        name: '0.'.green + ' Cancelar'
+    });
+
+    const preguntas = [
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Seleccione lugar:',
+            choices
+        }
+    ]
+
+    const { id } = await inquirer.prompt(preguntas);
+    return id;
+}
+
+const confirmar = async(message) => {
+
     const question = [
         {
-            type: 'input',
-            name: 'enter',
-            message: `Presione ${ 'enter'.green } para continuar`
+            type: 'confirm',
+            name: 'ok',
+            message
         }
     ];
 
-    console.log('\n');
-    await inquirer.prompt(question);
+    const { ok } = await inquirer.prompt(question);
+    return ok;
+}   
+
+const mostrarListadoChecklist = async( tareas = [] ) => {
+
+    const choices = tareas.map( (tarea, i) => {
+
+        const idx = `${i + 1}.`.green;
+
+        return {
+            value: tarea.id,
+            name:  `${ idx } ${ tarea.desc }`,
+            checked: ( tarea.completadoEn ) ? true : false
+        }
+    });
+
+    const pregunta = [
+        {
+            type: 'checkbox',
+            name: 'ids',
+            message: 'Selecciones',
+            choices
+        }
+    ]
+
+    const { ids } = await inquirer.prompt(pregunta);
+    return ids;
 }
+
 export  {
     inquirerMenu,
+    pausa,
     leerInput,
-    pausa
+    listarLugares,
+    confirmar,
+    mostrarListadoChecklist
 }
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import colors from 'colors';
 import { leerInput, inquirerMenu, pausa } from './helpers/inquirer.js';
+import Busquedas from './models/busquedas.js';
 
 const main = async () => {
+
+    const busquedas = new Busquedas();
     let opt = '';
+    
     do {
         opt = await inquirerMenu();
-        console.log({ opt });
 
-        await pausa();
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                console.log( lugar );
+                // Buscar los lugares
+                // Seleccionar el lugar
+                // Clima
+                // Mostar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad:',);
+                console.log('Lat:',);
+                console.log('Lng:',);
+                console.log('Temperatura:',);
+                console.log('Mínima:',);
+                console.log('Máxima:',);
+            break;
+        }
+
+        if (opt !== 0) await pausa();
     } while (opt !== 0);
 };
 
